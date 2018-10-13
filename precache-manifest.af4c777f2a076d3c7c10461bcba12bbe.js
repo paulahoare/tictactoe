d352af7d6e712ca2f71b63a909ee71a6de1f6a7d
Paula Hoare <paulahoare@gmail.com>
2018-10-13 15:16:30 -0400

    Updates

---
+++ precache-manifest.af4c777f2a076d3c7c10461bcba12bbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6a71d35c1fbb3241cd8",
+    "url": "/tictactoe/static/js/runtime~main.a6a71d35.js"
+  },
+  {
+    "revision": "7978ed23a83a068eb4ac",
+    "url": "/tictactoe/static/js/main.7978ed23.chunk.js"
+  },
+  {
+    "revision": "7e802ebce41dd84e0e5e",
+    "url": "/tictactoe/static/js/1.7e802ebc.chunk.js"
+  },
+  {
+    "revision": "7978ed23a83a068eb4ac",
+    "url": "/tictactoe/static/css/main.64ee7b28.chunk.css"
+  },
+  {
+    "revision": "b06dd6782af701b3d39ffd557aba669e",
+    "url": "/tictactoe/index.html"
+  }
+];
